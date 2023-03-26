485c4a334b4d94168790376fbe4a1dbed89bfcd2
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-03-26 17:36:44 +0700

    add user endpoints

--- src/crud/user.py
+++ src/crud/user.py
@@ -3,12 +3,22 @@ from sqlalchemy.orm import Session
 from .. import models, schemas
 
 
-def get_users(db: Session, skip: int = 0, limit: int = 100) -> list[models.User]:
+def get_one(db: Session, user_id: int) -> models.User:
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_one_by_name(db: Session, name: str) -> models.User:
+    return db.query(models.User).filter(models.User.name == name).first()
+
+
+def get_multiple(db: Session, skip: int = 0, limit: int = 100) -> list[models.User]:
     return db.query(models.User).offset(skip).limit(limit).all()
 
-def create_user(db: Session, user: schemas.UserCreate) -> models.User:
+
+def create_one(db: Session, user: schemas.UserCreate) -> models.User:
     db_user = models.User(name=user.name, password=user.password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+
     return db_user
--- src/main.py
+++ src/main.py
@@ -1,8 +1,49 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
-app = FastAPI()
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Pixel Code Platform",
+    description="Self-hostable coding courses/problems platform",
+    openapi_tags=[
+        {
+            "name": "users",
+            "description": "Operations with users",
+        },
+    ],
+)
 
 
-@app.get("/")
-def get_root() -> dict[str, str]:
-    return {"hello": "world"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users", response_model=schemas.UserRead, tags=["users"])
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.user.get_one_by_name(db=db, name=user.name)
+    if db_user:
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    return crud.user.create_one(db=db, user=user)
+
+
+@app.get("/users", response_model=list[schemas.UserRead], tags=["users"])
+def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.user.get_multiple(db=db, skip=skip, limit=limit)
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserRead, tags=["users"])
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.user.get_one(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return db_user
