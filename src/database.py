69fb6543fa9765def424d21867fd2a28eac01025
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-03-26 17:36:52 +0700

    log out ORM queries

--- src/database.py
+++ src/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    echo=True,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
