8baa6fab19383b6ebe0c6b6b646e87e366ad6794
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-03-26 19:23:41 +0700

    add typehint to declarative_base

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.decl_api import DeclarativeMeta
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
@@ -11,4 +12,4 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base: DeclarativeMeta = declarative_base()
