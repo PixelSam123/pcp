93c68db11e86fba5669d1c2a132ff87037d054a4
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-03-26 20:15:52 +0700

    add more tables

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -11,9 +11,13 @@ class User(Base):
     name = Column(String, unique=True, index=True)
     password = Column(String)
 
+    group_id = Column(Integer, ForeignKey("groups.id"))
     group = relationship("Group", back_populates="users")
 
-    challenges = relationship("Challenge", back_populates="author")
+    challenges = relationship("Challenge", back_populates="user")
+    submissions = relationship("Submission", back_populates="user")
+    challenge_comments = relationship("ChallengeComment", back_populates="user")
+    submission_comments = relationship("SubmissionComment", back_populates="user")
 
 
 class Group(Base):
@@ -34,4 +38,49 @@ class Challenge(Base):
     initial_code = Column(String)
     tier = Column(Integer)
 
-    author = relationship("User", back_populates="challenges")
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="challenges")
+
+    submissions = relationship("Submission", back_populates="challenge")
+    comments = relationship("ChallengeComment", back_populates="challenge")
+
+
+class Submission(Base):
+    __tablename__ = "submissions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String)
+    result = Column(String)
+    time = Column(Integer)
+    memory = Column(Integer)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="submissions")
+    challenge_id = Column(Integer, ForeignKey("challenges.id"))
+    challenge = relationship("Challenge", back_populates="submissions")
+
+    comments = relationship("SubmissionComment", back_populates="submission")
+
+
+class ChallengeComment(Base):
+    __tablename__ = "challenge_comments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="challenge_comments")
+    challenge_id = Column(Integer, ForeignKey("challenges.id"))
+    challenge = relationship("Challenge", back_populates="comments")
+
+
+class SubmissionComment(Base):
+    __tablename__ = "submission_comments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="submission_comments")
+    submission_id = Column(Integer, ForeignKey("submissions.id"))
+    submission = relationship("Submission", back_populates="comments")
