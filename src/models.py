528d440480d3166ebbb551a8cf9a964d9f8f7646
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-03-26 19:40:07 +0700

    add group (clan) and challenge types

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from .database import Base
 
@@ -9,3 +10,28 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
     password = Column(String)
+
+    group = relationship("Group", back_populates="users")
+
+    challenges = relationship("Challenge", back_populates="author")
+
+
+class Group(Base):
+    __tablename__ = "groups"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+
+    users = relationship("User", back_populates="group")
+
+
+class Challenge(Base):
+    __tablename__ = "challenges"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
+    initial_code = Column(String)
+    tier = Column(Integer)
+
+    author = relationship("User", back_populates="challenges")
