ad0750a96351f9a070d8cfe76eb15af430a20f51
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-03-31 12:35:44 +0700

    add challenge schema

---
+++ src/schemas/challenge.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+from . import user
+
+
+class Base(BaseModel):
+    name: str
+    tier: int
+
+
+class Create(Base):
+    user_id: int
+    description: str
+    initial_code: str
+
+
+class ReadBrief(Base):
+    id: int
+    user: user.ReadBrief
+
+    class Config:
+        orm_mode = True
+
+
+class Read(ReadBrief):
+    description: str
+    initial_code: str
+
+    class Config:
+        orm_mode = True
