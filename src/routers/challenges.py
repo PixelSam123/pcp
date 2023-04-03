c74782cfb180a5429c359ca762202b1d03c3c3d3
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-04-03 11:47:47 +0700

    add challenge GET endpoints

--- src/routers/challenges.py
+++ src/routers/challenges.py
@@ -26,3 +26,25 @@ def create_challenge(
         raise HTTPException(status_code=400, detail="User Doesn't Exist")
 
     return crud.challenge.create_one(db=db, challenge=challenge)
+
+
+@router.get("/", response_model=list[schemas.challenge.ReadBrief])
+def get_challenges(
+    skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+) -> list[models.Challenge]:
+    return crud.challenge.get_multiple(db=db, skip=skip, limit=limit)
+
+
+@router.get(
+    "/{challenge_name}",
+    response_model=schemas.challenge.Read,
+    responses=openapi_http_exception([(404, "Challenge Not Found")]),
+)
+def get_challenge_by_name(
+    challenge_name: str, db: Session = Depends(get_db)
+) -> models.Challenge:
+    db_challenge = crud.challenge.get_one_by_name(db=db, name=challenge_name)
+    if db_challenge is None:
+        raise HTTPException(status_code=404, detail="Challenge Not Found")
+
+    return db_challenge
