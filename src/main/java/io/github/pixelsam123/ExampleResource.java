0401266e234be101fca1be901a0a82077c666835
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-08-16 21:20:34 +0700

    change example resource to use IO threads

--- src/main/java/io/github/pixelsam123/ExampleResource.java
+++ src/main/java/io/github/pixelsam123/ExampleResource.java
@@ -1,5 +1,6 @@
 package io.github.pixelsam123;
 
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -7,10 +8,9 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class ExampleResource {
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    public Uni<String> hello() {
+        return Uni.createFrom().item("Hello from RESTEasy Reactive");
     }
 }
