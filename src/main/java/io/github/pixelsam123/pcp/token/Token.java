a31ad4fedc05d63b6d1f61720842568085fc2842
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-08-17 16:55:28 +0700

    convert Token type to record

--- src/main/java/io/github/pixelsam123/pcp/token/Token.java
+++ src/main/java/io/github/pixelsam123/pcp/token/Token.java
@@ -2,22 +2,8 @@ package io.github.pixelsam123.pcp.token;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Token {
-    private final String accessToken;
-    private final String tokenType;
-
-    public Token(String accessToken, String tokenType) {
-        this.accessToken = accessToken;
-        this.tokenType = tokenType;
-    }
-
-    @JsonProperty("access_token")
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    @JsonProperty("token_type")
-    public String getTokenType() {
-        return tokenType;
-    }
+public record Token(
+    @JsonProperty("access_token") String accessToken,
+    @JsonProperty("token_type") String tokenType
+) {
 }
