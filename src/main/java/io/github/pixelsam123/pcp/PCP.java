7eb9710644907552e8e82deef3688378c2c55518
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-09-01 11:26:23 +0700

    make OpenAPIDefinition work for the base app

--- src/main/java/io/github/pixelsam123/pcp/PCP.java
+++ src/main/java/io/github/pixelsam123/pcp/PCP.java
@@ -1,6 +1,7 @@
 package io.github.pixelsam123.pcp;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.core.Application;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
@@ -12,7 +13,7 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
         version = "1.0-SNAPSHOT"
     )
 )
-public class PCP {
+public class PCP extends Application {
     @ApplicationScoped
     public Argon2PasswordEncoder argon2PasswordEncoder() {
         return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
