23570708cd3668494c5878b138cdfc8b70e823ed
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-09-02 20:41:55 +0700

    submission resource changes

--- src/main/java/io/github/pixelsam123/pcp/SessionResource.java
+++ src/main/java/io/github/pixelsam123/pcp/SessionResource.java
@@ -1,5 +1,9 @@
-package io.github.pixelsam123.pcp.session;
+package io.github.pixelsam123.pcp;
 
+import io.github.pixelsam123.pcp.challenge.ChallengeBriefDto;
+import io.github.pixelsam123.pcp.challenge.ChallengeRepository;
+import io.github.pixelsam123.pcp.challenge.ChallengeSort;
+import io.github.pixelsam123.pcp.user.UserBriefDto;
 import io.github.pixelsam123.pcp.user.UserRepository;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.unchecked.Unchecked;
@@ -10,24 +14,28 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
 
 @Path("/session")
 public class SessionResource {
     private final String cookieName;
+    private final ChallengeRepository challengeRepository;
     private final UserRepository userRepository;
 
     public SessionResource(
         @ConfigProperty(name = "mp.jwt.token.cookie") String cookieName,
+        ChallengeRepository challengeRepository,
         UserRepository userRepository
     ) {
         this.cookieName = cookieName;
+        this.challengeRepository = challengeRepository;
         this.userRepository = userRepository;
     }
 
     @GET
     @RolesAllowed({"User"})
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Session> sessionGet(@Context SecurityContext ctx) {
+    public Uni<UserBriefDto> sessionUser(@Context SecurityContext ctx) {
         return userRepository
             .findByNameBrief(ctx.getUserPrincipal().getName())
             .map(Unchecked.function(dbUser -> {
@@ -35,8 +43,20 @@ public class SessionResource {
                     throw new NotFoundException("Username of your session is not found");
                 }
 
-                return new Session(dbUser.get());
+                return dbUser.get();
             }));
+    }
+
+    @GET
+    @Path("/challenges")
+    @RolesAllowed({"User"})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<List<ChallengeBriefDto>> sessionChallenges(@Context SecurityContext ctx) {
+        return challengeRepository.list(
+            List.of(1, 2, 3, 4, 5),
+            ctx.getUserPrincipal().getName(),
+            ChallengeSort.NEWEST
+        );
     }
 
     @POST
--- src/main/java/io/github/pixelsam123/pcp/session/Session.java
+++
@@ -1,8 +0,0 @@
-package io.github.pixelsam123.pcp.session;
-
-import io.github.pixelsam123.pcp.user.UserBriefDto;
-
-public record Session(
-    UserBriefDto userInfo
-) {
-}
