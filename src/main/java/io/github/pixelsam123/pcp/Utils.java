56e48736191e49ca211cb03f23e76c1644e9348a
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-09-09 21:01:09 +0700

    add javadoc to Utils

--- src/main/java/io/github/pixelsam123/pcp/Utils.java
+++ src/main/java/io/github/pixelsam123/pcp/Utils.java
@@ -11,6 +11,13 @@ public class Utils {
         throw new IllegalStateException("Utility class");
     }
 
+    /**
+     * Convenience function to check if multiple items in a tuple are equal.
+     * Useful for cross-checking of reactively fetched entity IDs.
+     *
+     * @param tuple tuples to check
+     * @return are all tuple items equal
+     */
     public static boolean areItemsEqual(Tuple tuple) {
         Object firstItem = tuple.nth(0);
 
@@ -23,6 +30,14 @@ public class Utils {
         return true;
     }
 
+    /**
+     * Convenience function to transform the given (unchecked) supplier into a regular supplier,
+     * then run it on the default worker pool.
+     *
+     * @param supplier the item supplier, must not be {@code null}, can produce {@code null}
+     * @param <T>      the type of item supplied by this supplier
+     * @return the new {@link Uni}
+     */
     public static <T> Uni<T> runInWorkerPool(UncheckedSupplier<T> supplier) {
         return Uni
             .createFrom()
